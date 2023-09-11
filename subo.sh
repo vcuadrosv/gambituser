ddb339c79432f94906ee2f10b3b7de6adae0e18b
vcuadrosv <valery.05@hotmail.com>
2023-09-10 21:50:06 -0500

    Ultimo Commit

--- subo.sh
+++ subo.sh
@@ -1,6 +1,17 @@
+#!/bin/bash
+
+# Agregar y confirmar los cambios en Git
 git add .
 git commit -m "Ultimo Commit"
-git push
-go build main.go 
-rm -f main.zip
+
+# Empaquetar el ejecutable en un archivo ZIP
+rm -f main.zip  # Eliminar el archivo ZIP anterior si existe
 zip main.zip main
+go build main.go 
+
+# Subir los cambios a GitHub
+git push
+
+
+
+
