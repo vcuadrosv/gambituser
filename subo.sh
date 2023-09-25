e9db58d141ee2752ea7bd9d29554fa8d3ae4f8e5
vcuadrosv <valery.05@hotmail.com>
2023-09-25 18:41:19 -0500

    Ultimo Commit

--- subo.sh
+++ subo.sh
@@ -7,7 +7,7 @@ git commit -m "Ultimo Commit"
 # Empaquetar el ejecutable en un archivo ZIP
 rm -f main.zip  # Eliminar el archivo ZIP anterior si existe
 zip main.zip main
-go build main.go 
+GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o main main.go
 
 # Subir los cambios a GitHub
 git push
